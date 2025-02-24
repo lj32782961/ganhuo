b08fc9742ca22a40e12f85222b17c2f22afea1bd
lj32782961 <lj32782961@gmail.com>
2025-02-24 14:30:02 +0100

    2025-02-24 14:30

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Article, Tag
 from django.http import HttpResponse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator,PageNotAnInteger, EmptyPage
 from django.db.models import Q
 
 def article_list(request):
@@ -20,24 +20,37 @@ def article_list(request):
 
     # 标签过滤
     if tag_filter:
-        articles = articles.filter(tag__name=tag_filter)
+        articles = articles.filter(tag__name=tag_filter).order_by('-pub_date')
 
     # 分页
     paginator = Paginator(articles, 10)  # 每页10条
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    # 防止在输入无效页码时程序崩溃。 现在如果输入的页码无效，则会显示第一页或最后一页的内容。
+    try:
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.get_page(1)
+    except EmptyPage:
+        page_obj = paginator.get_page(paginator.num_pages)
+
 
     #标签云
     all_tags = Tag.objects.all()
 
-    #return render(request, 'app/article_list.html', {'articles': articles})
-    return render(request, 'app/article_list.html', {
+    # 检查搜索结果是否为空
+    no_results = False
+    if not page_obj.object_list:
+        no_results = True
+
+    context = {
         'page_obj': page_obj,
         'search_query': search_query,
         'tag_filter': tag_filter,
-        'all_tags': all_tags
-    })
-
+        'all_tags': all_tags,
+        'no_results': no_results,
+    }
+    return render(request, 'app/article_list.html', context)
+    #return render(request, 'app/article_list.html', {'articles': articles})
 
 
 # def article_detail(request, pk):
